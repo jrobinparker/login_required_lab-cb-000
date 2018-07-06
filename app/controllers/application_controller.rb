743c7d8af72792fe1dd3c2a1dc466bec538facd9
Jeremy Parker <jrobinparker@gmail.com>
2018-07-06 21:53:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def authentication_required 
+  def current_user 
     if !logged_in? 
       redirect_to root_path 
     end 
