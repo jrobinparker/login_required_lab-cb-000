e92bed37986b6a70f1107f6ac4ddf70abec7d6ac
Jeremy Parker <jrobinparker@gmail.com>
2018-07-06 21:20:37 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   
   def create 
     user = User.find_by(params[:name])
-    session[:user_id] = user.id
+    session[:name] = user.id
     re
   end 
   
