67f246e23d92a80605a2d0e31f89477e54005483
Jeremy Parker <jrobinparker@gmail.com>
2018-07-06 22:05:17 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   
   def create 
     @user = User.find_by(name: params[:name])
-    session[:id] = @user.name 
+    session[:user_id] = @user.name 
     redirect_to "secrets/show"
   end 
 
